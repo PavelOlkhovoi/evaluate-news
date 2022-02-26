e9a13b628bdbf961f402854419351c8d712494f8
Pavel <p-olhovoi@bk.ru>
2022-02-26 17:34:11 +0100

    chore: Update comments on all js functions

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,45 +1,56 @@
+/**
+* @description Form Handler evokes validate and fetch API functions 
+* @param {event} Click - Should be a button 
+*/
 function formHandler(event){
     event.preventDefault();
     let linkInput = document.getElementById('link');
     let link = linkInput.value;
     let errorBox = document.querySelector('.main__error');
     let testPattern = formValidate(linkInput.value);
-
     if(testPattern) {
         errorBox.innerHTML = ` `;
         // Get key for API reguest from server
         const key = getKey().then(res => res)
         // Fetch data to api.meaningcloud.com
         .then(res=> {
-            hideResult();
+            hideResult();       
             const test = fetchData('https://api.meaningcloud.com/sentiment-2.1', {
                 key: res.key,
                 lang:"en",
                 url:link
             });
         });
-        return true;
     }else {
         errorBox.innerHTML = `Bed link`;
-        return false;
     }
-    
 }
-
+/**
+* @description Form validate 
+* @param {string} url - The link that should be evaluated
+* @returns {boolean} - Tru / false 
+*/
 function formValidate(linkInput){
     // Simple regexp to check the first part of the link
     let test = /^https{0,1}:\/\/+\w*\.+.*/.test(linkInput);
-    console.log('Status', test);
     return test;
 }
+/**
+* @description The get request to the internal server to get the API key 
+* @returns {object} Key - Custom object that will send on server
+*/
 async function getKey(){
     const response = await fetch('http://localhost:8081/key');
     const data = await response.json();
 
     return data
 }
+/**
+* @description The post request to API Meaning cloud
+* @param {string} url - https://api.meaningcloud.com/sentiment-2.1
+* @param {object} data - Params of the Request
+*/
 async function fetchData(url, data={}){
-    
     fetch(url,
     {
         method: "POST",
@@ -60,15 +71,24 @@ async function fetchData(url, data={}){
     })
     .catch(error => console.log('error', error));
 }
-
+/**
+* @description Hide the results block 
+*/
 function hideResult(){
     document.querySelector('.reply').classList.remove('active');
 }
 
+/**
+* @description Show the results block 
+*/
 function showResult(){
     document.querySelector('.reply').classList.add('active');
 }
 
+/**
+* @description Send data from API to APP
+* @param {object} - Object with data
+*/
 function renderData(resultObj){
     for (let key in resultObj){
         document.getElementById(key).innerHTML = `${key}: <em>${resultObj[key]}</em>`;
